29dee28f3f1cf288ed5daa9458c05fdf39bf2768
Alex Gorji <aligorji@hotmail.com>
2019-02-20 19:40:35 +0100

    test_score

--- musicscore/tree/tests/test_score.py
+++ musicscore/tree/tests/test_score.py
@@ -11,3 +11,23 @@ class TestScore(TestCase):
         self.score.add_part(Part(name='new name', print_object='yes'))
         self.score.add_part(Part(id='p10'))
         print(self.score.partwise.to_string())
+        self.score.partwise.test_mode = True
+        result ='''<score-partwise>
+  <part-list>
+    <score-part>
+      <part-name/>
+    </score-part>
+    <score-part>
+      <part-name/>
+    </score-part>
+    <score-part>
+      <part-name/>
+    </score-part>
+  </part-list>
+  <part/>
+  <part/>
+  <part/>
+</score-partwise>
+'''
+        # print(self.score.partwise.to_string())
+        self.assertEqual(self.score.partwise.to_string(), result)
