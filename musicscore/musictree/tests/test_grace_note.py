408e2efded3c4659b423bc4dafa0aaca41d5a744
Alex Gorji <aligorji@hotmail.com>
2019-04-23 18:03:08 +0200

    too slow but works

--- musicscore/musictree/tests/test_grace_note.py
+++ musicscore/musictree/tests/test_grace_note.py
@@ -1,7 +1,11 @@
 from unittest import TestCase
+import os
 
 from musicscore.musictree.treechord import TreeChord
 from musicscore.musictree.treescore_timewise import TreeScoreTimewise
+from musicscore.musicxml.score_templates.xml_test_score import TestScore
+
+path = os.path.abspath(__file__).split('.')[0]
 
 
 class Test(TestCase):
@@ -9,19 +13,10 @@ class Test(TestCase):
         self.score = TreeScoreTimewise()
 
     def test_1(self):
-        ch = TreeChord(quarter_duration=0)
-        print(ch.get_children())
-
-
-    def test_2(self):
         self.score.add_measure()
         self.score.add_part()
         self.score.add_chord(1, 1, TreeChord())
         self.score.add_chord(1, 1, TreeChord(quarter_duration=0))
-
-
-        # p.quantize()
-        # print([chord.quarter_duration for chord in p.chords])
-        # print(self.score.get_measure(1).get_part(1).chords[1].get_children())
-
-        print(self.score.to_string())
+        result_path = path + '_test_1'
+        self.score.write(path=result_path)
+        TestScore().assert_template(result_path=result_path)
