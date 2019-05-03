a1fcb1c758d2a8c6296d7763bf1d9796b1c95159
Alex Gorji <aligorji@hotmail.com>
2019-05-03 08:36:34 +0200

    __init__

---
+++ musicscore/__init__.py
@@ -0,0 +1,4 @@
+import musicscore.fractaltree.fractaltree
+import musicscore.permutation.permutation
+
+from musicscore.musictree import treenote, treepart, treebeat, treechord, treemeasure, treescore_timewise, treescorepart, treetime
