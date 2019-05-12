a82e51894fa25d789592e01f6ff2ca83e3c99496
Alex Gorji <aligorji@hotmail.com>
2019-05-12 19:11:26 +0200

    wip

--- musicscore/musictree/tests/test_adjoin_ties.py
+++ musicscore/musictree/tests/test_adjoin_ties.py
@@ -12,7 +12,7 @@ class Test(TestCase):
     def setUp(self) -> None:
         self.score = TreeScoreTimewise()
 
-    def make_test(self, *args):
+    def make_t(self, *args):
         sf = SimpleFormat(durations=[1, 1, 1, 1])
         for i in range(3):
             sf.chords[i].is_adjoinable = args[i]
@@ -28,49 +28,49 @@ class Test(TestCase):
         self.score.add_subtitle(str([chord.is_adjoinable for chord in sf.chords]))
 
     def test_1(self):
-        self.make_test(False, False, False)
+        self.make_t(False, False, False)
         result_path = path + '_test_1'
         self.score.write(path=result_path)
         TestScore().assert_template(result_path=result_path)
 
     def test_2(self):
-        self.make_test(False, False, True)
+        self.make_t(False, False, True)
         result_path = path + '_test_2'
         self.score.write(path=result_path)
         TestScore().assert_template(result_path=result_path)
 
     def test_3(self):
-        self.make_test(False, True, False)
+        self.make_t(False, True, False)
         result_path = path + '_test_3'
         self.score.write(path=result_path)
         TestScore().assert_template(result_path=result_path)
 
     def test_4(self):
-        self.make_test(False, True, True)
+        self.make_t(False, True, True)
         result_path = path + '_test_4'
         self.score.write(path=result_path)
         TestScore().assert_template(result_path=result_path)
 
     def test_5(self):
-        self.make_test(True, False, False)
+        self.make_t(True, False, False)
         result_path = path + '_test_5'
         self.score.write(path=result_path)
         TestScore().assert_template(result_path=result_path)
 
     def test_6(self):
-        self.make_test(True, False, True)
+        self.make_t(True, False, True)
         result_path = path + '_test_6'
         self.score.write(path=result_path)
         TestScore().assert_template(result_path=result_path)
 
     def test_7(self):
-        self.make_test(True, True, False)
+        self.make_t(True, True, False)
         result_path = path + '_test_7'
         self.score.write(path=result_path)
         TestScore().assert_template(result_path=result_path)
 
     def test_8(self):
-        self.make_test(True, True, True)
+        self.make_t(True, True, True)
         result_path = path + '_test_8'
         self.score.write(path=result_path)
         TestScore().assert_template(result_path=result_path)
