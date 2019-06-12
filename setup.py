24701c55f82f0ada55574a722077feaa391a5370
Alex Gorji <aligorji@hotmail.com>
2019-06-12 18:02:36 +0200

    wip

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ import setuptools
 
 setuptools.setup(
     name="musicscore-alexgorji",
-    version="0.0.3",
+    version="0.0.4",
     author="Alex Gorji",
     author_email="aligorji@hotmail.com",
     description="generating musicxml",
     url="https://github.com/alexgorji/musicscore.git",
     packages=setuptools.find_packages(),
+    install_requires=['lxml==4.3.1',
+                      'quicktions==1.9'
+                      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
