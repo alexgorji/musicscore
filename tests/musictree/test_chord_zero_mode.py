9ef421aa516d8f37a2f954252e64f2f48ec8bbfd
Alex Gorji <aligorji@hotmail.com>
2019-09-09 16:46:27 +0300

    wip

---
+++ tests/musictree/test_chord_zero_mode.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from musicscore.musicstream import SimpleFormat
+from musicscore.musictree.treescoretimewise import TreeScoreTimewise
+import os
+
+from tests.score_templates.xml_test_score import TestScore
+
+path = os.path.abspath(__file__).split('.')[0]
+
+
+class Test(TestCase):
+    def setUp(self) -> None:
+        self.score = TreeScoreTimewise()
+
+    def test_1(self):
+        sf = SimpleFormat(durations=[1, 0, 3, 0, 1])
+        sf.chords[1].zero_mode = 'remove'
+
+        sf.to_stream_voice().add_to_score(self.score)
+        xml_path = path + '_test_1.xml'
+        self.score.write(xml_path)
+        TestScore().assert_template(xml_path)
