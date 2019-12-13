d005895281041b53c7c3486e4149a2f0e5d59d63
Alex Gorji <aligorji@hotmail.com>
2019-12-13 20:47:44 +0100

    wip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="musicscore",
-    version="0.0.1.8",
+    version="0.0.1.9",
     author="Alex Gorji",
     author_email="aligorji@hotmail.com",
     description="generating musicxml",
