0e5acf0da9d9230fccce9dd6a5ca7dda9c8eec1e
Alex Gorji <aligorji@hotmail.com>
2020-01-28 12:52:31 +0100

    v1.0.1

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ containing musicxml, musictree, musicstream
 see documentation on
 https://github.com/alexgorji/musicscore.git  
 
+
+v 1.0.1:
+TreeScoreTimewise().extend: bug fixed: \_add_identification() deleted
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="musicscore",
-    version="1.0.0",
+    version="1.0.1",
     author="Alex Gorji",
     author_email="aligorji@hotmail.com",
     description="generating musicxml",
