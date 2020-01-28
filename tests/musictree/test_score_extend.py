a46b1be44c067ee39bd498edd57c9cf3089a2ea4
Alex Gorji <aligorji@hotmail.com>
2020-01-28 12:23:51 +0100

    test

--- tests/musictree/test_score_extend.py
+++ tests/musictree/test_score_extend.py
@@ -1,5 +1,5 @@
-from unittest import TestCase
 import os
+from unittest import TestCase
 
 from musicscore.musicstream.streamvoice import SimpleFormat
 from musicscore.musictree.treescoretimewise import TreeScoreTimewise
@@ -29,3 +29,21 @@ class Test(TestCase):
         xml_path = path + '_test_1.xml'
         self.main_score.write(path=xml_path)
         TestScore().assert_template(result_path=xml_path)
+
+    def test_2(self):
+        section_score_1 = TreeScoreTimewise()
+        sf = SimpleFormat(durations=2 * [5, 4, 3, 2, 1])
+        section_score_1.set_time_signatures(durations=sum(sf.durations), barline_style='light-light')
+        sf.to_stream_voice().add_to_score(section_score_1)
+
+        section_score_2 = TreeScoreTimewise()
+        sf = SimpleFormat(durations=3 * [1, 2, 3, 4, 5])
+        section_score_2.set_time_signatures(durations=sum(sf.durations), barline_style='heavy')
+        sf.to_stream_voice().add_to_score(section_score_2)
+
+        self.main_score.extend(section_score_1)
+        self.main_score.extend(section_score_2)
+
+        xml_path = path + '_test_2.xml'
+        self.main_score.write(path=xml_path)
+        # TestScore().assert_template(result_path=xml_path)
