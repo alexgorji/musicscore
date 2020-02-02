57150ad32e05a61b8452be28edb4e4d42f534df3
Alex Gorji <aligorji@hotmail.com>
2020-02-02 17:39:56 +0100

    part_group

--- musicscore/musictree/treepartgroup.py
+++
@@ -1,6 +0,0 @@
-from musicscore.musicxml.types.complextypes.partlist import PartGroup
-
-
-class TreePartGroup(PartGroup):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
