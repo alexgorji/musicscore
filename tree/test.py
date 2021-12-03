e621a32b9b06e217325a006c2a302a6a6c844bcb
alexgorji <aligorji@hotmail.com>
2021-12-03 20:41:02 +0100

    module tree with class TreePresentation added

---
+++ tree/test.py
@@ -0,0 +1,67 @@
+from unittest import TestCase
+
+from tree.tree import TreePresentation
+
+
+class A(TreePresentation):
+    def __init__(self, name, parent=None):
+        self._children = []
+        self._parent = parent
+        self.name = name
+
+    def add_child(self, name):
+        child = type(self)(parent=self, name=name)
+        self._children.append(child)
+        return child
+
+    def get_children(self):
+        return self._children
+
+    def get_parent(self):
+        return self._parent
+
+
+class TestTree(TestCase):
+    def setUp(self) -> None:
+        self.root = A(name='root')
+        self.child1 = self.root.add_child('child1')
+        self.child2 = self.root.add_child('child2')
+        self.child3 = self.root.add_child('child3')
+        self.child4 = self.root.add_child('child4')
+        self.grandchild1 = self.child2.add_child('grandchild1')
+        self.grandchild2 = self.child2.add_child('grandchild2')
+        self.grandchild3 = self.child4.add_child('grandchild3')
+        self.greatgrandchild1 = self.grandchild2.add_child('greatgrandchild1')
+
+    def test_get_root(self):
+        assert self.greatgrandchild1.get_root() == self.root
+        assert self.child4.get_root() == self.root
+
+    def test_is_leaf(self):
+        assert self.greatgrandchild1.is_leaf is True
+        assert self.child4.is_leaf is False
+        assert self.child3.is_leaf is True
+
+    def test_traverse(self):
+        assert list(self.root.traverse()) == [self.root, self.child1, self.child2, self.grandchild1, self.grandchild2,
+                                              self.greatgrandchild1, self.child3, self.child4, self.grandchild3]
+
+    def test_iterate_leaves(self):
+        assert list(self.root.iterate_leaves()) == [self.child1, self.grandchild1, self.greatgrandchild1,
+                                                    self.child3, self.grandchild3]
+
+    def test_get_layer_number(self):
+        assert [node.get_layer_number() for node in self.root.traverse()] == [0, 1, 1, 2, 2, 3, 1, 1, 2]
+
+    def test_print_tree(self):
+        expected = """root
+    child1
+    child2
+        grandchild1
+        grandchild2
+            greatgrandchild1
+    child3
+    child4
+        grandchild3
+"""
+        assert self.root.print_tree('name') == expected
---
+++ tree/tree.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod, abstractproperty
+
+
+class TreePresentation(ABC):
+    @property
+    def is_leaf(self):
+        if not self.get_children():
+            return True
+        else:
+            return False
+
+    def iterate_leaves(self):
+        for node in self.traverse():
+            if node.is_leaf:
+                yield node
+
+    @abstractmethod
+    def get_children(self):
+        pass
+
+    def traverse(self):
+        if self is not None:
+            yield self
+            for child in self.get_children():
+                for node in child.traverse():
+                    yield node
+
+    def get_root(self):
+        node = self
+        parent = node.get_parent()
+        while parent is not None:
+            node = parent
+            parent = node.get_parent()
+        return node
+
+    def get_layer_number(self):
+        output = 0
+        node = self
+        while node.get_parent() is not None:
+            output += 1
+            node = node.get_parent()
+        return output
+
+    @abstractmethod
+    def get_parent(self):
+        pass
+
+    def print_tree(self, attr):
+        def _indentation(x):
+            indentation = ''
+            for i in range(x.get_layer_number()):
+                indentation += '    '
+            return indentation
+
+        """
+        A string representation of the tree structure
+        """
+        output = ''
+        for node in self.traverse():
+            output += _indentation(node) + getattr(node, attr)
+            output += '\n'
+
+        return output
