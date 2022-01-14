c452efdb5df0732c29d87693a1efa128a1e60851
alexgorji <aligorji@hotmail.com>
2022-01-14 18:22:39 +0100

    test for xml with randomly scrumbles children of notes in parser.

--- musicxml/parser/parser.py
+++ musicxml/parser/parser.py
@@ -30,21 +30,13 @@ def _et_xml_to_music_xml(node):
 
 
 def _parse_node(xml_node):
-    # print('parsing node:', xml_node.tag, xml_node.attrib)
     output = _et_xml_to_music_xml(xml_node)
-    # print('output', output)
     for child in xml_node:
         output.add_child(_parse_node(child))
     return output
 
 
 def parse_musicxml(file_path):
-    # start_et = datetime.datetime.now()
     with open(file_path) as file:
         xml = ET.parse(file)
-    end_et = datetime.datetime.now()
-    # print(f'parse musicxml ET parsing: {end_et - start_et}')
-    parsed = _parse_node(xml.getroot())
-    # end_parsing = datetime.datetime.now()
-    # print(f'parse musicxml parsing: {end_parsing - end_et}')
-    return parsed
+    return _parse_node(xml.getroot())
