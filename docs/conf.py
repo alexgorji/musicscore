b878ff54445d48009b47795049022d8f322a12fb
alexgorji <aligorji@hotmail.com>
2022-03-07 11:38:42 +0100

    docs.conf path to musicxml

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import sys
 
 sys.path.insert(0, Path(__file__).parents[1].resolve().as_posix())
+sys.path.insert(0, (Path(__file__).parents[1].resolve()/'musicxml').as_posix())
 
 # -- Project information -----------------------------------------------------
 
