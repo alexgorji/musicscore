a2222e5f46c6b507c789482d779e9f7c45b56e3e
alexgorji <aligorji@hotmail.com>
2024-06-20 13:52:27 +0200

    dev-ver-2.2.0beta branch created

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 long_description = (Path(__file__).parent / "README.rst").read_text()
 setuptools.setup(
     name="musicscore",
-    version="2.1.0",
+    version="2.2.0beta",
     author="Alex Gorji",
     author_email="aligorji@hotmail.com",
     description="Generating musicxml files.",
